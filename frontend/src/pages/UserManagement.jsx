983baab2946c37d291d3970c454b4de2ac86f830
Next71 <info@nextelbpo.co>
2026-01-28 22:44:56 +0500

    ,

--- frontend/src/pages/UserManagement.jsx
+++ frontend/src/pages/UserManagement.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useAuth } from '../../context/AuthContext.jsx';
+import { useAuth } from '../context/AuthContext';
 import {
   Users,
   Search,
